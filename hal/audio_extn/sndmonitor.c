fd8f6c3f42b16411f48d2802743b6eb14a1e2087
Aditya Bavanari <abavanar@codeaurora.org>
2019-01-28 16:33:39 +0530

    hal: sndmonitor: Add support for trinket sound card in sndmonitor
    Add support for trinket sound card variants in sndmonitor
    to enable SSR and PDR in trinket target.
    
    Change-Id: I4691c76362b626a2295727bb6b050fcfeaec791b

--- hal/audio_extn/sndmonitor.c
+++ hal/audio_extn/sndmonitor.c
@@ -195,7 +195,8 @@ static int enum_sndcards()
             (strncasecmp(card_id, "sdm", 3) != 0) &&
             (strncasecmp(card_id, "sdc", 3) != 0) &&
             (strncasecmp(card_id, "apq", 3) != 0) &&
-            (strncasecmp(card_id, "sm8150", 6) != 0)) {
+            (strncasecmp(card_id, "sm8150", 6) != 0) &&
+            (strncasecmp(card_id, "trinket", 7) != 0)) {
             ALOGW("Skip over non-ADSP snd card %s", card_id);
             continue;
         }
